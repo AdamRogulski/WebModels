a069fa45d7a19f74cd36b69dc389e72de052dd36
Adam <adamwrogulski@gmail.com>
2019-12-10 13:09:24 +0100

    initial commit

---
+++ src/test/java/com/example/WebModels/WebModelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WebModels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebModelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
